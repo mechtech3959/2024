7b321155a0253e85e5d566a204800c8decf4b94a
Alex Posey <alxposey@gmail.com>
2024-02-08 16:50:25 -0600

    add new branch

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,86 +1,141 @@
-#include "ctre/Phoenix.h"
-#include "frc/TimedRobot.h"
-#include "frc/XboxController.h"
-#include "frc/drive/DifferentialDrive.h"
+#include "Robot.h"
+#include <iostream>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
 
-using namespace frc;
+#include <string>
 
-class Robot : public TimedRobot {
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <ctre/Phoenix.h>
+// #include <frc/SpeedControllerGroup.h>
+#include <frc/XboxController.h>
+#include <frc/drive/DifferentialDrive.h>
+
+class Robot : public frc::TimedRobot
+{
 public:
-  WPI_TalonSRX _driveRightFront{1};
-  WPI_TalonSRX _driveRightFollower{2};
-  WPI_TalonSRX _driveLeftFront{3};
-  WPI_TalonSRX _driveLeftFollower{4};
-  WPI_TalonSRX _launcherLeftFront{5};
-  WPI_TalonSRX _launcherLeftFollower{6};
-  WPI_TalonSRX _launcherRightFront{7};
-  WPI_TalonSRX _launcherRightFollower{8};
-  WPI_TalonSRX _intake{9};
-  WPI_TalonSRX _climber{10};
 
-  DifferentialDrive _diffDrive{_driveLeftFront, _driveRightFront};
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
 
-  frc::XboxController _controller{0};
+  WPI_TalonSRX m_Left0[1];
+  WPI_TalonSRX m_Left1[2];
+  WPI_TalonSRX m_Right0[3];
+  WPI_TalonSRX m_Right1[4];
 
-  void TeleopPeriodic() {
 
-    std::stringstream work;
+  frc::DifferentialDrive m_Drive{m_Left0, m_Right0};
+  frc::XboxController m_Controller;
 
-    double forw = -_controller.GetLeftY();
-    double spin = _controller.GetLeftX();
-    double launcherSpeed = _controller.GetRightTriggerAxis();
-    bool launcherReverse = _controller.GetRightBumper();
-    bool intake = _controller.GetLeftBumper();
-    bool intakeReverse = _controller.GetAButton();
+  bool m_LimelightHasTarget;
+  double m_LimelightTurnCmd;
+  double m_LimelightDriveCmd;
 
-    if (fabs(forw) < 0.10)
-      forw = 0;
-    if (fabs(spin) < 0.10)
-      spin = 0;
 
-    _diffDrive.ArcadeDrive(forw, spin, false);
+double clamp(double in, double minval, double maxval)
+{
+  if (in > maxval)
+    return maxval;
+  if (in < minval)
+    return minval;
+  return in;
+}
 
-    if (launcherReverse)
-      launcherSpeed = -launcherSpeed;
-    _launcherRightFront.Set(motorcontrol::TalonSRXControlMode::PercentOutput,
-                            launcherSpeed);
 
-    if (intake)
-      (intakeReverse)
-          ? _intake.Set(motorcontrol::TalonSRXControlMode::PercentOutput, -100)
-          : _intake.Set(motorcontrol::TalonSRXControlMode::PercentOutput, 100);
+
+void RobotInit()
+{
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+}
+
+void RobotPeriodic() {}
+
+void AutonomousInit() {}
+
+void AutonomousPeriodic() {}
+
+void TeleopInit() {}
+
+void TeleopPeriodic()
+{
+  Update_Limelight_Tracking();
+
+  bool do_limelight = m_Controller.GetAButton();
+  if (do_limelight)
+  {
+    if (m_LimelightHasTarget)
+    {
+      m_Drive.ArcadeDrive(m_LimelightDriveCmd, m_LimelightTurnCmd);
+    }
+    else
+    {
+      m_Drive.ArcadeDrive(0.0, 0.0);
+    }
   }
+  else
+  {
+    // Tank Drive
+    // double left = -m_Controller.GetY(frc::GenericHID::JoystickHand::kLeftHand);
+    // double right = -m_Controller.GetY(frc::GenericHID::JoystickHand::kRightHand);
+    // m_Drive.TankDrive(left,right);
 
-  void RobotInit() {
-    // Set all motors to factory defaults
-    _driveRightFront.ConfigFactoryDefault();
-    _driveRightFollower.ConfigFactoryDefault();
-    _driveLeftFront.ConfigFactoryDefault();
-    _driveLeftFollower.ConfigFactoryDefault();
-    _launcherLeftFront.ConfigFactoryDefault();
-    _launcherLeftFollower.ConfigFactoryDefault();
-    _launcherRightFront.ConfigFactoryDefault();
-    _launcherRightFront.ConfigFactoryDefault();
-
-    // Set up followers
-    _driveRightFollower.Follow(_driveRightFront);
-    _driveLeftFollower.Follow(_driveLeftFront);
-    _launcherLeftFollower.Follow(_launcherLeftFront);
-    _launcherRightFollower.Follow(_launcherRightFront);
-    _launcherRightFront.Follow(_launcherLeftFront);
-
-    // Invert the right side motors
-    _driveRightFront.SetInverted(true);
-    _driveRightFollower.SetInverted(true);
-    _driveLeftFront.SetInverted(false);
-    _driveLeftFollower.SetInverted(false);
-
-    // Set TalonSRX LEDs to be the correct colors
-    _driveRightFront.SetSensorPhase(true);
-    _driveLeftFront.SetSensorPhase(true);
+    // Arcade Drive
+    double fwd = -m_Controller.GetLeftY();
+    double turn = m_Controller.GetLeftX();
+    turn *= 0.7f;
+    m_Drive.ArcadeDrive(fwd, turn);
   }
+}
 
-private:
+void TestPeriodic() {}
+
+void Update_Limelight_Tracking()
+{
+  // Proportional Steering Constant:
+  // If your robot doesn't turn fast enough toward the target, make this number bigger
+  // If your robot oscillates (swings back and forth past the target) make this smaller
+  const double STEER_K = 0.05;
+
+  // Proportional Drive constant: bigger = faster drive
+  const double DRIVE_K = 0.26;
+
+  // Area of the target when your robot has reached the goal
+  const double DESIRED_TARGET_AREA = 13.0;
+  const double MAX_DRIVE = 0.65;
+  const double MAX_STEER = 1.0f;
+
+  std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+  double tx = table->GetNumber("tx", 0.0);
+  double ty = table->GetNumber("ty", 0.0);
+  double ta = table->GetNumber("ta", 0.0);
+  double tv = table->GetNumber("tv", 0.0);
+
+  if (tv < 1.0)
+  {
+    m_LimelightHasTarget = false;
+    m_LimelightDriveCmd = 0.0;
+    m_LimelightTurnCmd = 0.0;
+  }
+  else
+  {
+    m_LimelightHasTarget = true;
+
+    // Proportional steering
+    m_LimelightTurnCmd = tx * STEER_K;
+    m_LimelightTurnCmd = clamp(m_LimelightTurnCmd, -MAX_STEER, MAX_STEER);
+
+    // drive forward until the target area reaches our desired area
+    m_LimelightDriveCmd = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
+    m_LimelightDriveCmd = clamp(m_LimelightDriveCmd, -MAX_DRIVE, MAX_DRIVE);
+  }
+}
+
 };
 
 #ifndef RUNNING_FRC_TESTS
