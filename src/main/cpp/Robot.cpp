9ae17b0b829c717d73cf3b5aa31e3ddf5735801a
Alex Posey <alxposey@gmail.com>
2024-02-08 19:43:52 -0600

    uhh

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -26,7 +26,10 @@ public:
   WPI_TalonSRX m_Left1{2};
   WPI_TalonSRX m_Right0{3};
   WPI_TalonSRX m_Right1{4};
- 
+ //m_Left0
+ //m_Left1
+// m_Right0
+// m_Right1
   frc::DifferentialDrive m_Drive {m_Left0, m_Right0};
   frc::XboxController m_Controller{0};
 
@@ -45,56 +48,6 @@ double clamp(double in, double minval, double maxval)
 };
 
 Robot(){}
-
-
-void RobotInit()   override
-{
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-}
-
-void RobotPeriodic()   override{}
-
-void AutonomousInit()   override{}
-
-void AutonomousPeriodic()   override{}
-
-void TeleopInit()   override{}
-
-void TeleopPeriodic()   override
-{
-  Update_Limelight_Tracking();
-if(m_Controller.GetAButton()){
-  bool do_limelight = true;   
-  if (do_limelight)
-  {
-    if (m_LimelightHasTarget)
-    {
-      m_Drive.ArcadeDrive(m_LimelightDriveCmd, m_LimelightTurnCmd);
-    }
-    else
-    {
-      m_Drive.ArcadeDrive(0.0, 0.0);
-    }
-  }
-  else
-  {
-    // Tank Drive
-    // double left = -m_Controller.GetY(frc::GenericHID::JoystickHand::kLeftHand);
-    // double right = -m_Controller.GetY(frc::GenericHID::JoystickHand::kRightHand);
-    // m_Drive.TankDrive(left,right);
-
-    // Arcade Drive
-    double fwd = -m_Controller.GetLeftY();
-    double turn = m_Controller.GetLeftX();
-    turn *= 0.7f;
-    m_Drive.ArcadeDrive(fwd, turn);
-  }};
-}
-
-void TestPeriodic()  override {}
-
 void Update_Limelight_Tracking()    
 {
   // Proportional Steering Constant:
@@ -138,7 +91,55 @@ void Update_Limelight_Tracking()
   }
 }
 
+void RobotInit()   override
+{ 
+  
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+}
+
+void RobotPeriodic()   override{}
+
+void AutonomousInit()   override{}
+
+void AutonomousPeriodic()   override{}
+
+void TeleopInit()   override{}
+
+void TeleopPeriodic()   override
+{
+  Update_Limelight_Tracking();
+if(m_Controller.GetAButton()){
+    if (m_LimelightHasTarget)
+    {
+      m_Drive.ArcadeDrive(m_LimelightDriveCmd, m_LimelightTurnCmd);
+    }
+    else
+    {
+      m_Drive.ArcadeDrive(0.0, 0.0);
+    }
+  }
+  else
+  {
+    // Tank Drive
+    // double left = -m_Controller.GetY(frc::GenericHID::JoystickHand::kLeftHand);
+    // double right = -m_Controller.GetY(frc::GenericHID::JoystickHand::kRightHand);
+    // m_Drive.TankDrive(left,right);
+
+    // Arcade Drive
+    double fwd = -m_Controller.GetLeftY();
+    double turn = m_Controller.GetLeftX();
+    turn *= 0.7f;
+    m_Drive.ArcadeDrive(fwd, turn);
+  }};
+
+
+void TestPeriodic()  override {}
+
+ 
 };
+
 
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }
