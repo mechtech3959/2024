70bdb4f9de9a2f9b081575f7012b01cdf2dff7b9
Zac <zacharytayloy0508@gmail.com>
2024-02-12 22:06:48 -0600

    AGGhHHH

--- src/main/include/WayPointPoses.h
+++ src/main/include/WayPointPoses.h
@@ -4,7 +4,7 @@
 #include <frc/trajectory/TrajectoryGenerator.h>
 #include <frc/trajectory/constraint/MaxVelocityConstraint.h>
 #include <frc/trajectory/constraint/RectangularRegionConstraint.h>
-
+ 
 #include <units/acceleration.h>
 #include <units/angular_velocity.h>
 #include <units/dimensionless.h>
@@ -21,9 +21,9 @@ constexpr units::inch_t BlueTapeX = 231.2_in;
 constexpr units::inch_t TapeOffset = 0_in;
 // https://firstfrc.blob.core.windows.net/frc2024/FieldAssets/2024FieldDrawings.pdf
 // pg 4 for april ID
-constexpr units::inch_t sourceC = 9.68_in; // april tag #1 & #10 far from wall
-constexpr units::inch_t sourceF =
-    34.79_in; // april tag #2 & #9 close to the wall
+constexpr units::inch_t sourceB_farY = 9.68_in; // april tag #1   
+constexpr units::inch_t sourceB_CloseY = 34.79_in; // aprile tag #2
+constexpr units::inch_t sourceB_CloseY =34.79_in; // april tag #9 & #9 close to the wall
 constexpr units::inch_t speakerS =
     196.17_in; // april tag #3 source side of the red station
 constexpr units::inch_t speakerA = 218.42_in; // april tag #4 amp side
@@ -40,3 +40,15 @@ constexpr units::inch_t RedYOffSetLeft = 0_in;  // prob same from last year
 
 constexpr units::degree_t RedHeading = 0_deg;      // prob same from last year
 constexpr units::degree_t BlueHeading = 179.9_deg; // prob same from last year
+
+
+constexpr frc::Translation2d BlueTop{ 250.50_in, 260.64_in}; // right of the field avoiding stage and goes to midfield
+constexpr frc::Translation2d BlueBottom{ 250.50_in, 29.64_in}; // left of the field avoiding stage and goes to midfield
+// I did this backwards but it works the same so shut up
+constexpr frc::Translation2d MidfieldNote5{ 250.50_in, 29.64_in};// bottom
+constexpr frc::Translation2d MidfieldNote4{ 250.50_in, 95.64_in};
+constexpr frc::Translation2d MidfieldNote3{ 250.50_in,  161.64_in};
+constexpr frc::Translation2d MidfieldNote2{ 250.50_in,  227.64_in};
+constexpr frc::Translation2d MidfieldNote1{ 250.50_in,  293.64_in};//top
+
+
