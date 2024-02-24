ad214ea06ba8cd4767a61455a3ef8e9190713be1
Alex Posey <alxposey@gmail.com>
2024-02-24 11:26:23 -0600

    Lots of changes

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,34 +1,88 @@
 #include "Robot.h"
 #include "LimeLight.h"
 #include <ctre/phoenix6/Orchestra.hpp>
+#include <frc/AddressableLED.h>
 TalonFX MUSIC{12};
+
+static constexpr int kLength = 60;
+
+frc::AddressableLED m_led{1};
+
+std::array<frc::AddressableLED::LEDData, kLength>
+
+    m_ledBuffer; // Reuse the buffer
+
+// Store what the last hue of the first pixel is
+
+int firstPixelHue = 0;
+
 void Robot::ampAuto() { // zac did this :)
   limelight.updateTracking();
-  if (autoTimer.Get() < 3.0_s) {
-    drive.diffDrive.ArcadeDrive(0.6, limelight.m_LimelightTurnCmd);
-  } else if (autoTimer.Get() > 3.0_s && autoTimer.Get() < 6.0_s) {
-    shooter.ShootAmp();
-  } else if (autoTimer.Get() > 6.0_s && autoTimer.Get() < 8_s) {
-    drive.diffDrive.ArcadeDrive(-0.6, -0.4);
-    intake.Forward();
-  } else if (autoTimer.Get() > 8.0_s && autoTimer.Get() < 10_s) {
-    drive.diffDrive.ArcadeDrive(0.6, 0.4);
-    intake.Forward();
-  } else if (autoTimer.Get() > 10.0_s && autoTimer.Get() < 15_s) {
-    shooter.ShootAmp();
+  if (frc::DriverStation::GetAlliance() ==
+      frc::DriverStation::Alliance::kBlue) {
+    if (autoTimer.Get() < 3.0_s) {
+      drive.diffDrive.ArcadeDrive(
+          0.6, limelight.m_LimelightTurnCmd); // drive towards apriltag
+    } else if (autoTimer.Get() > 3.0_s && autoTimer.Get() < 6.0_s) {
+      shooter.ShootAmp(); // shoot
+    } else if (autoTimer.Get() > 6.0_s && autoTimer.Get() < 8_s) {
+      drive.diffDrive.ArcadeDrive(-0.65, -0.45); // turn to note and intake it
+      intake.Forward();
+    } else if (autoTimer.Get() > 8.0_s && autoTimer.Get() < 10_s) {
+      drive.diffDrive.ArcadeDrive(0.65, 0.45); // turn to amp
+      intake.Forward();
+    } else if (autoTimer.Get() > 10.0_s && autoTimer.Get() < 15_s) {
+      shooter.ShootAmp(); // score
+    }
+  } else if (frc::DriverStation::GetAlliance() ==
+             frc::DriverStation::Alliance::kRed) { // red
+    if (autoTimer.Get() < 3.0_s) {
+      drive.diffDrive.ArcadeDrive(0.6, limelight.m_LimelightTurnCmd);
+    } else if (autoTimer.Get() > 3.0_s && autoTimer.Get() < 6.0_s) {
+      shooter.ShootAmp();
+    } else if (autoTimer.Get() > 6.0_s && autoTimer.Get() < 8_s) {
+      drive.diffDrive.ArcadeDrive(-0.65, 0.45);
+      intake.Forward();
+    } else if (autoTimer.Get() > 8.0_s && autoTimer.Get() < 10_s) {
+      drive.diffDrive.ArcadeDrive(0.65, -0.45);
+      intake.Forward();
+    } else if (autoTimer.Get() > 10.0_s && autoTimer.Get() < 15_s) {
+      shooter.ShootAmp();
+    }
+  } else {
+    // cout << "no" << endl;
+    drive.diffDrive.ArcadeDrive(0.0, 0.0);
   }
 }
 
 void Robot::middleAuto() { // callie did this :D
-  if (autoTimer.Get() < 3.0_s) {
-    shooter.ShootSpeaker();
-  } else if (autoTimer.Get() > 3.0_s && autoTimer.Get() < 5.0_s) {
-    drive.diffDrive.ArcadeDrive(-0.5, 0.0);
-    intake.Forward();
-  } else if (autoTimer.Get() > 5.0_s && autoTimer.Get() < 7.0_s) {
-    drive.diffDrive.ArcadeDrive(0.5, 0.0);
-  } else if (autoTimer.Get() > 7.0_s && autoTimer.Get() < 10.0_s) {
-    shooter.ShootSpeaker();
+  if (frc::DriverStation::GetAlliance() ==
+      frc::DriverStation::Alliance::kBlue) {
+    if (autoTimer.Get() < 3.0_s) {
+      shooter.ShootSpeaker();
+    } else if (autoTimer.Get() > 3.0_s && autoTimer.Get() < 5.0_s) {
+      drive.diffDrive.ArcadeDrive(-0.6, 0.0);
+      intake.Forward();
+    } else if (autoTimer.Get() > 5.0_s && autoTimer.Get() < 7.5_s) {
+      drive.diffDrive.ArcadeDrive(0.6, 0.0);
+    } else if (autoTimer.Get() > 7.5_s && autoTimer.Get() < 10.0_s) {
+      shooter.ShootSpeaker();
+    }
+  } else if (frc::DriverStation::GetAlliance() ==
+             frc::DriverStation::Alliance::kRed) {
+    if (autoTimer.Get() < 3.0_s) {
+      shooter.ShootSpeaker();
+    } else if (autoTimer.Get() > 3.0_s && autoTimer.Get() < 5.0_s) {
+      drive.diffDrive.ArcadeDrive(-0.6, 0.0);
+      intake.Forward();
+    } else if (autoTimer.Get() > 5.0_s && autoTimer.Get() < 7.5_s) {
+      drive.diffDrive.ArcadeDrive(0.6, 0.0);
+    } else if (autoTimer.Get() > 7.5_s && autoTimer.Get() < 10.0_s) {
+      shooter.ShootSpeaker();
+    }
+  } else {
+    // cout << "no" << endl;
+    drive.diffDrive.ArcadeDrive(0, 0);
   }
 }
 
@@ -36,17 +90,32 @@ void Robot::sideAuto() { // do not delete jeron pls
   if (autoTimer.Get() < 3.0_s) {
     shooter.ShootSpeaker();
   } else if (autoTimer.Get() > 3.0_s && autoTimer.Get() < 5.0_s) {
-    drive.diffDrive.ArcadeDrive(-0.5, -0.5);
+    drive.diffDrive.ArcadeDrive(-0.65, -0.45);
     intake.Forward();
   } else if (autoTimer.Get() > 5.0_s && autoTimer.Get() < 7.0_s) {
-    drive.diffDrive.ArcadeDrive(0.5, 0.5);
+    drive.diffDrive.ArcadeDrive(0.65, 0.45);
     intake.Forward();
   } else if (autoTimer.Get() > 7.0_s && autoTimer.Get() < 10.0_s) {
     shooter.ShootSpeaker();
   }
 }
+
 // auto selector
 void Robot::RobotInit() {
+  // Default to a length of 60, start empty output
+
+  // Length is expensive to set, so only set it once, then just update data
+
+  m_led.SetLength(kLength);
+  m_led.SetData(m_ledBuffer);
+  m_led.Start();
+
+  for (int wow = 0; wow < kLength; wow++) {
+    m_ledBuffer[wow].SetRGB(255, 0, 0);
+  }
+
+  m_led.SetData(m_ledBuffer);
+
   m_autoChooser.SetDefaultOption(a_AmpAuto, AutoRoutine::kAmpAuto);
   m_autoChooser.AddOption(a_MiddleAuto, AutoRoutine::kMiddleAuto);
   m_autoChooser.AddOption(a_SideAuto, AutoRoutine::kSideAuto);
@@ -113,6 +182,14 @@ void Robot::AutonomousPeriodic() {
 }
 
 void Robot::TeleopPeriodic() {
+
+  for (int i = 0; i < kLength; i++) {
+    m_ledBuffer[i].SetRGB(255, 0, 0);
+  }
+
+  m_led.SetData(m_ledBuffer);
+  m_led.Start();
+
   // Get inputs/controller mapping
   double forw = -_controller.GetLeftY();
   double spin = _controller.GetRightX();
@@ -198,11 +275,28 @@ void Robot::TeleopPeriodic() {
 void Robot::DisabledInit() {
   autoTimer.Stop();
   autoTimer.Reset();
+  m_led.SetLength(kLength);
+
+  m_led.SetData(m_ledBuffer);
 }
 
-void Robot::RobotPeriodic() {}
+void Robot::RobotPeriodic() {
+  for (int i = 0; i < kLength; i++) {
+    m_ledBuffer[i].SetRGB(255, 0, 0);
+  }
+
+  m_led.SetData(m_ledBuffer);
+  m_led.Start();
+}
 void Robot::TeleopInit() {}
-void Robot::DisabledPeriodic() {}
+void Robot::DisabledPeriodic() {
+  for (int i = 0; i < kLength; i++) {
+    m_ledBuffer[i].SetRGB(255, 0, 0);
+  }
+
+  m_led.SetData(m_ledBuffer);
+  m_led.Start();
+}
 void Robot::SimulationInit() {}
 void Robot::SimulationPeriodic() {}
 void Robot::TestPeriodic() {}
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -12,29 +12,31 @@
 #include "Shooter.h"
 #include "TankDrive.h"
 #include <ctre/Phoenix.h>
+#include <frc/DriverStation.h>
 #include <frc/TimedRobot.h>
 #include <frc/XboxController.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/smartdashboard/SendableChooser.h>
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <iostream>
 #include <rev/CANSparkMax.h>
 #include <units/pressure.h>
 #include <units/time.h>
-
-// Unneeded headers
-// Leaving them here for future use
-// #include <frc2/command/CommandPtr.h>
-// #include <frc/MathUtil.h>
-// #include <frc/filter/SlewRateLimiter.h>
-// #include "ctre/phoenix/motorcontrol/ControlMode.h"
-// #include <frc/Compressor.h>
-// #include <frc/trajectory/Trajectory.h>
-// #include <frc/trajectory/TrajectoryGenerator.h>
-// #include <frc/MathUtil.h>
-// #include <units/math.h>
-// #include <frc/DriverStation.h>
-// #include "LimeLight.h"
-// #include <units/pressure.h>
+// using namespace std;
+//  Unneeded headers
+//  Leaving them here for future use
+//  #include <frc2/command/CommandPtr.h>
+//  #include <frc/MathUtil.h>
+//  #include <frc/filter/SlewRateLimiter.h>
+//  #include "ctre/phoenix/motorcontrol/ControlMode.h"
+//  #include <frc/Compressor.h>
+//  #include <frc/trajectory/Trajectory.h>
+//  #include <frc/trajectory/TrajectoryGenerator.h>
+//  #include <frc/MathUtil.h>
+//  #include <units/math.h>
+//  #include <frc/DriverStation.h>
+//  #include "LimeLight.h"
+//  #include <units/pressure.h>
 
 class Robot : public frc::TimedRobot {
 private:
@@ -46,6 +48,16 @@ private:
   TankDrive drive{};
   LimeLight limelight{"limelight-greenie"};
 
+  /* static constexpr int kLength = 60;
+
+   // PWM port 9
+   // Must be a PWM header, not MXP or DIO
+   frc::AddressableLED m_led{0};
+   std::array<frc::AddressableLED::LEDData, kLength>
+       m_ledBuffer; // Reuse the buffer
+   // Store what the last hue of the first pixel is
+   int firstPixelHue = 0;
+ */
   // Everything from here until the public block is auto stuff
   // I have no idea how it works, ask Zac
   /*  frc::SendableChooser<std::string> m_chooser;
